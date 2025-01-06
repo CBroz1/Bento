3c286a04677288838f76cd57e60696e43d82d458
CBroz1 <CBrozdowski@yahoo.com>
2025-01-06 10:28:40 -0600

    Change links

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: '😴 Zzz',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: '556a26f6ae1100e020a564e6ee54ea0e', // Write here your API Key
@@ -50,83 +50,83 @@ const CONFIG = {
 	// ├┴┐│ │ │  │ │ ││││└─┐
 	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
 
-	firstButtonsContainer: [
-		{
-			id: '1',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/u/0/r?pli=1',
-		},
-		{
-			id: '2',
-			name: 'Mail',
-			icon: 'mail',
-			link: 'https://mail.yahoo.com/d/search/referrer=unread&keyword=is%253Aunread&accountIds=1&excludefolders=ARCHIVE',
-		},
-		{
-			id: '3',
-			name: 'YouTube',
-			icon: 'youtube',
-			link: 'https://www.youtube.com/feed/subscriptions',
-		},
-		{
-			id: '4',
-			name: 'Weather',
-			icon: 'cloud-moon',
-			link: 'https://www.accuweather.com/en/us/logan-square/60647/hourly-weather-forecast/2094562',
-		},
-		{
-			id: '5',
-			name: 'Search',
-			icon: 'search',
-			link: 'https://www.google.com/',
-		},
-		{
-			id: '6',
-			name: 'Fire',
-			icon: 'flame-kindling',
-			link: 'https://www.youtube.com/watch?v=L_LUpnjgPso',
-		},
-	],
-
-	secondButtonsContainer: [
-		{
-			id: '1',
-			name: 'Music',
-			icon: 'headphones',
-			link: 'https://open.spotify.com',
-		},
-		{
-			id: '2',
-			name: 'twitter',
-			icon: 'twitter',
-			link: 'https://twitter.com/',
-		},
-		{
-			id: '3',
-			name: 'bot',
-			icon: 'bot',
-			link: 'https://discord.com/app',
-		},
-		{
-			id: '4',
-			name: 'Amazon',
-			icon: 'shopping-bag',
-			link: 'https://amazon.com/',
-		},
-		{
-			id: '5',
-			name: 'Hashnode',
-			icon: 'pen-tool',
-			link: 'https://hashnode.com/',
-		},
-		{
-			id: '6',
-			name: 'Figma',
-			icon: 'figma',
-			link: 'https://figma.com/',
-		},
-	],
+	// firstButtonsContainer: [
+	// 	{
+	// 		id: '1',
+	// 		name: 'Calendar',
+	// 		icon: 'calendar',
+	// 		link: 'https://calendar.google.com/calendar/u/0/r?pli=1',
+	// 	},
+	// 	{
+	// 		id: '2',
+	// 		name: 'Mail',
+	// 		icon: 'mail',
+	// 		link: 'https://mail.yahoo.com/d/search/referrer=unread&keyword=is%253Aunread&accountIds=1&excludefolders=ARCHIVE',
+	// 	},
+	// 	{
+	// 		id: '3',
+	// 		name: 'YouTube',
+	// 		icon: 'youtube',
+	// 		link: 'https://www.youtube.com/feed/subscriptions',
+	// 	},
+	// 	{
+	// 		id: '4',
+	// 		name: 'Weather',
+	// 		icon: 'cloud-moon',
+	// 		link: 'https://www.accuweather.com/en/us/logan-square/60647/hourly-weather-forecast/2094562',
+	// 	},
+	// 	{
+	// 		id: '5',
+	// 		name: 'Search',
+	// 		icon: 'search',
+	// 		link: 'https://www.google.com/',
+	// 	},
+	// 	{
+	// 		id: '6',
+	// 		name: 'Fire',
+	// 		icon: 'flame-kindling',
+	// 		link: 'https://www.youtube.com/watch?v=L_LUpnjgPso',
+	// 	},
+	// ],
+	//
+	// secondButtonsContainer: [
+	// 	{
+	// 		id: '1',
+	// 		name: 'Music',
+	// 		icon: 'headphones',
+	// 		link: 'https://open.spotify.com',
+	// 	},
+	// 	{
+	// 		id: '2',
+	// 		name: 'twitter',
+	// 		icon: 'twitter',
+	// 		link: 'https://twitter.com/',
+	// 	},
+	// 	{
+	// 		id: '3',
+	// 		name: 'bot',
+	// 		icon: 'bot',
+	// 		link: 'https://discord.com/app',
+	// 	},
+	// 	{
+	// 		id: '4',
+	// 		name: 'Amazon',
+	// 		icon: 'shopping-bag',
+	// 		link: 'https://amazon.com/',
+	// 	},
+	// 	{
+	// 		id: '5',
+	// 		name: 'Hashnode',
+	// 		icon: 'pen-tool',
+	// 		link: 'https://hashnode.com/',
+	// 	},
+	// 	{
+	// 		id: '6',
+	// 		name: 'Figma',
+	// 		icon: 'figma',
+	// 		link: 'https://figma.com/',
+	// 	},
+	// ],
 
 	// ┬  ┬┌─┐┌┬┐┌─┐
 	// │  │└─┐ │ └─┐
@@ -135,8 +135,30 @@ const CONFIG = {
 	// First Links Container
 	firstlistsContainer: [
 		{
-			icon: 'music',
+			icon: 'briefcase-business',
 			id: '1',
+			links: [
+				{
+					name: 'Cal',
+					link: 'https://calendar.google.com/calendar/u/0/r?pli=1',
+				},
+				{
+					name: 'git',
+					link: 'https://github.com/notifications',
+				},
+				{
+					name: 'yah',
+					link: 'https://mail.yahoo.com/d/search/referrer=unread&keyword=is%253Aunread&accountIds=1&excludefolders=ARCHIVE',
+				},
+				{
+					name: '🌩️',
+					link: 'https://www.accuweather.com/en/us/logan-square/60647/hourly-weather-forecast/2094562',
+				},
+			],
+		},
+		{
+			icon: 'music',
+			id: '2',
 			links: [
 				{
 					name: 'Chill',
@@ -156,17 +178,43 @@ const CONFIG = {
 				},
 			],
 		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'square-play',
+			id: '1',
+			links: [
+				{
+					name: 'yt',
+					link: 'https://www.youtube.com/feed/subscriptions',
+				},
+				{
+					name: 'neb',
+					link: 'https://nebula.tv/explore/videos',
+				},
+				{
+					name: 'drop',
+					link: 'https://www.dropout.tv/new-releases',
+				},
+				{
+					name: '🔥',
+					link: 'https://www.youtube.com/watch?v=IoNE2dycFkA',
+				},
+			],
+		},
 		{
 			icon: 'tv',
 			id: '2',
 			links: [
 				{
-					name: 'Dropout',
-					link: 'https://www.dropout.tv/new-releases',
+					name: 'Apple',
+					link: 'https://tv.apple.com/',
 				},
 				{
-					name: 'Nebula',
-					link: 'https://nebula.tv/explore/videos',
+					name: 'Disney',
+					link: 'https://www.disneyplus.com/',
 				},
 				{
 					name: 'Max',
@@ -175,54 +223,6 @@ const CONFIG = {
 				{
 					name: 'Hulu',
 					link: 'https://www.hulu.com/',
-				},
-			],
-		},
-	],
-
-	// Second Links Container
-	secondListsContainer: [
-		{
-			icon: 'binary',
-			id: '1',
-			links: [
-				{
-					name: 'Spotify',
-					link: 'https://www.spotify.com',
-				},
-				{
-					name: 'Reddit',
-					link: 'https://www.reddit.com',
-				},
-				{
-					name: 'Hashnode',
-					link: 'https://www.hashnode.com',
-				},
-				{
-					name: 'Pocket',
-					link: 'https://www.pocket.com',
-				},
-			],
-		},
-		{
-			icon: 'github',
-			id: '2',
-			links: [
-				{
-					name: 'Front',
-					link: 'https://www.reddit.com/r/Frontend/',
-				},
-				{
-					name: 'Rust',
-					link: 'https://www.reddit.com/r/rust/',
-				},
-				{
-					name: 'Go',
-					link: 'https://www.reddit.com/r/golang/',
-				},
-				{
-					name: 'Repos',
-					link: 'https://github.com/migueravila',
 				},
 			],
 		},
